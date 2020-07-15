40a30e013f64fb7bef17438a28619d6223bca84c
renjie <rj.aiyayao@gmail.com>
2020-07-15 10:33:05 +0800

    restore code

--- src/setting.js
+++ src/setting.js
@@ -1,9 +1,9 @@
 
 const config = {
-  APIToken: "a616d945e5afcb8c5a23d9ce22a84a6eaf66bf07",
+  APIToken: "",
   server: "https://dev.seafile.com/dtable-web",
-  workspaceID: "5",
-  dtableName: "插件测试",
+  workspaceID: "",
+  dtableName: "",
   lang: "zh-cn"
 };
 

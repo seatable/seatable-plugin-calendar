4e4651ddc11885c68a01fad3bc82695c6cf17a19
renjie-run <rj.aiyayao@gmail.com>
2021-01-28 15:12:38 +0800

    update translation

--- src/locale/lang/de.js
+++ src/locale/lang/de.js
@@ -5,9 +5,9 @@ const de = {
   "Select_date_column": "Datumsspalte wählen",
   "Select_an_end_date_column": "Spalte mit Enddatum wählen",
   "Select_color_column": "Spalte für Farbe wählen",
-  "Start_Date": "Startdatum",
-  "End_Date_Optional": "Enddatum (optional)",
-  "Color_From": "Farbe",
+  "Start_Date": "Start date field",
+  "End_Date_Optional": "End date field (optional)",
+  "Color_From": "Color field",
   "Using_xxx_column": "Spalte \"{columnName}\" verwenden",
   "Year": "Jahr",
   "Month": "Monat",
@@ -35,20 +35,18 @@ const de = {
   "Unnamed_record": "Neuer Eintrag",
   "more": "mehr",
   "xxx_Month_xxx_Date_xxx_Week": "{weekDay} {month} {date}",
-  "No_date_field_to_place_records_on_the_calendar": "Keine Datumsinformationen vorhanden, um die Einträge im Kalender anzuzeigen.",
-  "There_are_no_records": "Es gibt keine Einträge",
+  "No_date_field_to_place_records_on_the_calendar": "Es sind keine Datumsinformationen vorhanden, um die Einträge im Kalender anzuzeigen.",
+  "There_are_no_records": "Es gibt keine Einträge.",
   "Today": "Heue",
-
   "New_View": "Neue Ansicht",
   "Rename_View": "Ansicht umbenennen",
   "Delete_View": "Ansicht löschen",
   "Name": "Name",
-  "Name_is_required": "Name ist erforderlich",
-
+  "Name_is_required": "Der Name ist eine Pflichtangabe.",
   "Settings": "Einstellungen",
   "Table": "Tabelle",
-  "Title": "",
   "View": "Ansicht",
+  "Title": "Title field",
   "No_options": "Keine Optionen"
 };
 

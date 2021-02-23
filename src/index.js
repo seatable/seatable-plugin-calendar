679233a49f1e6754a262366096402cec2c909f6f
Tom Klingenberg <tklingenberg@lastflood.net>
2021-02-23 16:21:05 +0100

    build: dtable-ui window.app.expandRow() dev shim
    the window.app.expandRow() function is in use to open the "Row Details"
    editor in the calendar and other plugins, see [ref-sdk].
    
    in a development setup this part of the dtable-sdk dtable-ui API is not
    available.
    
    adding a shim in the development setup allows to track app.expandRow()
    initiations via the browser console.
    
    ref-sdk: https://docs.seatable.io/published/dtable-sdk/dtable-ui.md
    forum: https://forum.seatable.io/t/how-does-row-details-work-in-the-calendar-plugin/321

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ import './setting';
 
 ReactDOM.render(<App showDialog={true} key={(new Date()).getTime()} />, document.getElementById('root'));
 
+/**
+ * plugin development window.app shim (dtable-ui)
+ *
+ * @link https://docs.seatable.io/published/dtable-sdk/dtable-ui.md
+ */
+window.app = window.app ? window.app : {};
+window.app.expandRow = window.app.expandRow || ((row, rowTable) => {
+  console.log('dtable-ui shim: app.expandRow', [row, rowTable]);
+});

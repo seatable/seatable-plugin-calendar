f898c6cc10c8919efc44f19cdb15b461acc4ed1a
Tom Klingenberg <tklingenberg@lastflood.net>
2021-03-30 17:09:32 +0200

    build: cs-fix
    running the build with the CI environment parameter true made it fail.
    
        $ CI=true npm run build
    
    fix is to make it pass.

--- src/ReactBigCalendar.js
+++ src/ReactBigCalendar.js
@@ -136,7 +136,7 @@ class ReactBigCalendar extends React.Component {
     const date = startDateColumnType === 'formula' ? rawRow[startDateColumnName] : row[startDateColumnKey];
     const endDate = endDateColumnType === 'formula' ? rawRow[endDateColumnName] : row[endDateColumnKey];
     if (!date) return null; // start date must exist.
-    
+
     // invalid event if duration less than 0 between end date with start date.
     if (endDate && moment(endDate).isBefore(date)) {
       return null;

2bcab16964a261af248af669a3facb900873ed4a
Tom Klingenberg <352517+ktomk@users.noreply.github.com>
2021-04-06 11:45:45 +0200

    deselect end-date (#32)
    * deselect end-date
    
    the end-date is optional, this change-set adds an empty first option to
    the select-box to allow the user to deselect it again.
    
    * 1.use "select a field" to display an empty option.
    2.add field icon.
    3.generate drop-down options in the constructor stage.
    
    * update style
    
    * change "select a field" to "Not used"
    
    * remove useless code
    
    Co-authored-by: renjie-run <rj.aiyayao@gmail.com>

--- src/components/view-setting.js
+++ src/components/view-setting.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment }  from 'react';
 import PropTypes from 'prop-types';
 import intl from 'react-intl-universal';
 import PluginSelect from './plugin-select';
@@ -12,6 +12,7 @@ const propTypes = {
   views: PropTypes.array,
   columns: PropTypes.array,
   CellType: PropTypes.object,
+  columnIconConfig: PropTypes.object,
   settings: PropTypes.object,
   onModifyViewSettings: PropTypes.func,
   toggleViewSettingPanel: PropTypes.func
@@ -21,9 +22,42 @@ class ViewSetting extends React.Component {
 
   constructor(props) {
     super(props);
+    const { tables, views } = props;
     this.state = {
       settings: props.settings || {},
     };
+    const { dateColumns, colorColumns, titleColumns } = this.getSelectorColumns();
+    this.tableOptions = this.createOptions(tables, SETTING_KEY.TABLE_NAME, 'name');
+    this.viewOptions = this.createOptions(views, SETTING_KEY.VIEW_NAME, 'name');
+    this.titleColumnOptions = this.createOptions(titleColumns, SETTING_KEY.COLUMN_TITLE, 'value');
+    this.dateColumnOptions = this.createOptions(dateColumns, SETTING_KEY.COLUMN_START_DATE, 'value');
+    this.endDateColumnOptions = this.createOptions(dateColumns, SETTING_KEY.COLUMN_END_DATE, 'value');
+    if (this.endDateColumnOptions.length) {
+      this.endDateColumnOptions.unshift(
+        {
+          value: '',
+          setting_key: SETTING_KEY.COLUMN_END_DATE,
+          label: <span className={'select-module select-module-name null-option-name'}>{intl.get('Not_used')}</span>,
+        }
+      );
+    }
+    this.colorColumnOptions = this.createOptions(colorColumns, SETTING_KEY.COLUMN_COLOR, 'value');
+  }
+
+  createOptions(source, settingKey, valueKey) {
+    if (!Array.isArray(source)) {
+      return [];
+    }
+    return source.map((item) => ({
+      value: item[valueKey],
+      setting_key: settingKey,
+      label: (
+        <Fragment>
+          {item.iconClass && <span className="header-icon"><i className={item.iconClass}></i></span>}
+          <span className='select-module select-module-name'>{item.name}</span>
+        </Fragment>
+      ),
+    }));
   }
 
   onModifySettings = (selectedOption) => {
@@ -46,7 +80,7 @@ class ViewSetting extends React.Component {
   };
 
   getSelectorColumns = () => {
-    const { columns, CellType } = this.props;
+    const { columns, CellType, columnIconConfig } = this.props;
     let dateColumns = [],
       colorColumns = [],
       titleColumns = [];
@@ -54,36 +88,34 @@ class ViewSetting extends React.Component {
       CellType.TEXT, CellType.SINGLE_SELECT, CellType.FORMULA,
       CellType.COLLABORATOR, CellType.CREATOR, CellType.LAST_MODIFIER];
     columns && columns.forEach((c) => {
-      const { type } = c;
-      if (type === CellType.DATE) {
-        dateColumns.push(c);
-      } else if (type === CellType.FORMULA && c.data.result_type === 'date') {
-        dateColumns.push(c);
+      const { type, name } = c;
+      const columnOption = {
+        name,
+        value: name,
+        iconClass: columnIconConfig[type],
+      };
+      if (type === CellType.DATE || (type === CellType.FORMULA && c.data.result_type === 'date')) {
+        dateColumns.push(columnOption);
       } else if (type === CellType.SINGLE_SELECT) {
-        colorColumns.push(c);
-      }
-      if (titleColumnTypes.indexOf(type) !== -1) {
-        titleColumns.push(c);
+        colorColumns.push(columnOption);
+      } else if (titleColumnTypes.includes(type)) {
+        titleColumns.push(columnOption);
       }
     });
     return { dateColumns, colorColumns, titleColumns };
   }
 
-  renderSelector = (source, settingKey, valueKey, labelKey) => {
+  renderSelector = (options, settingKey) => {
     let { settings } = this.state;
-    let options = source.map((item) => {
-      let value = item[valueKey];
-      let label = item[labelKey];
-      return {value, label, setting_key: settingKey};
-    });
     let selectedOption = options.find(item => item.value === settings[settingKey]);
     if (!selectedOption && (
       settingKey === SETTING_KEY.TABLE_NAME ||
       settingKey === SETTING_KEY.VIEW_NAME ||
       settingKey === SETTING_KEY.COLUMN_TITLE)) {
-      selectedOption = options[0];
+      selectedOption = options[0] || undefined;
     }
     return <PluginSelect
+      classNamePrefix={'calendar-view-setting-selector'}
       value={selectedOption}
       options={options}
       onChange={this.onModifySettings}
@@ -91,8 +123,6 @@ class ViewSetting extends React.Component {
   }
 
   render() {
-    const { tables, views } = this.props;
-    const { dateColumns, colorColumns, titleColumns } = this.getSelectorColumns();
 
     return (
       <div className="plugin-view-setting position-absolute d-flex flex-column" style={{zIndex: 4}} ref={ref => this.ViewSetting = ref}>
@@ -106,27 +136,27 @@ class ViewSetting extends React.Component {
           <div className="setting-list">
             <div className="setting-item table-setting">
               <div className="title">{intl.get('Table')}</div>
-              {this.renderSelector(tables, SETTING_KEY.TABLE_NAME, 'name', 'name')}
+              {this.renderSelector(this.tableOptions, SETTING_KEY.TABLE_NAME)}
             </div>
             <div className="setting-item view-setting">
               <div className="title">{intl.get('View')}</div>
-              {this.renderSelector(views, SETTING_KEY.VIEW_NAME, 'name', 'name')}
+              {this.renderSelector(this.viewOptions, SETTING_KEY.VIEW_NAME)}
             </div>
             <div className="setting-item view-setting">
               <div className="title">{intl.get('Title')}</div>
-              {this.renderSelector(titleColumns, SETTING_KEY.COLUMN_TITLE, 'name', 'name')}
+              {this.renderSelector(this.titleColumnOptions, SETTING_KEY.COLUMN_TITLE)}
             </div>
             <div className="setting-item">
               <div className="title">{intl.get('Start_Date')}</div>
-              {this.renderSelector(dateColumns, SETTING_KEY.COLUMN_START_DATE, 'name', 'name')}
+              {this.renderSelector(this.dateColumnOptions, SETTING_KEY.COLUMN_START_DATE)}
             </div>
             <div className="setting-item">
               <div className="title">{intl.get('End_Date_Optional')}</div>
-              {this.renderSelector(dateColumns, SETTING_KEY.COLUMN_END_DATE, 'name', 'name')}
+              {this.renderSelector(this.endDateColumnOptions, SETTING_KEY.COLUMN_END_DATE)}
             </div>
             <div className="setting-item">
               <div className="title">{intl.get('Color_From')}</div>
-              {this.renderSelector(colorColumns, SETTING_KEY.COLUMN_COLOR, 'name', 'name')}
+              {this.renderSelector(this.colorColumnOptions, SETTING_KEY.COLUMN_COLOR)}
             </div>
             <p className="small text-muted">{intl.get('Calendar_Select_Description')}</p>
           </div>

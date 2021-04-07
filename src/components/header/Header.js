e7812f9104f0e3b797489e6014b2f87a53342e83
Tom Klingenberg <tklingenberg@lastflood.net>
2021-04-07 17:59:49 +0200

    move localizer out of header
    small refactoring to build-back Header component (upstream) moving the
    localizer and intl to places of use (label is just a property).
    
    renders isShowWeek flag void and untangles label translations.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import intl from 'react-intl-universal';
 
-const Header = ({ label, isShowWeek }) => {
-  return <span>{isShowWeek ? intl.get('Week_xxx', {weekNumber: intl.get(label)}) : intl.get(label)}</span>;
-};
+const Header = ({ label }) => (
+  <span role="columnheader" aria-sort="none">
+    {label}
+  </span>
+);
 
 Header.propTypes = {
   label: PropTypes.node,
-  isShowWeek: PropTypes.bool,
 };
 
 export default Header;

15db220fa7a5eae64a3e29575bff8a6ce3269ee1
Tom Klingenberg <tklingenberg@lastflood.net>
2021-04-19 11:17:15 +0200

    build: improve cs-fix locale/lang/*.js
    since 3962387 the .js locale files templates with the eslint style so the
    files are clean when generated.
    
    improvement is in the template to handle white-space more generously when
    editing files by hand and adding more/less whitespace in the .json file.
    
    ref: 3962387556eb5b226048bff14e0c2008853513a8

--- scripts/translate.js
+++ scripts/translate.js
@@ -10,7 +10,7 @@ const pluginName = files[0];
 
 const langTemplate = ([...s], lang, content) => `${s.shift()}${lang}${s.shift()}${
   content.replace(
-    /^(\s*)"(.*?)": "(.*)",?$/gm
+    /^(\s*)"(.*?)"\s*:\s*"(.*)"\s*,?\s*$/gm
     , (m, p1, p2, p3) => `${p1}'${p2}': '${p3.replace(/'/g, '\\$&')}',`
   ).trimEnd()
 }${s.shift()}${lang}${s.shift()}`;

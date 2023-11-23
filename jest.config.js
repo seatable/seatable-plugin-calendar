9450a680303860db37698aebeee9baef0dcaf666
renjie-run <rj.aiyayao@gmail.com>
2023-11-23 10:25:51 +0800

    update code

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.resolve(__dirname, './'),
+  roots: ['<rootDir>/tests/'],
+  testMatch: ['<rootDir>/tests/**/(*.)+(spec|test).[jt]s?(x)'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+  },
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(css|less)$': '<rootDir>/config/jest/cssTransform.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$',
+  ],
+}

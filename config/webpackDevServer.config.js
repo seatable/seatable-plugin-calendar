17bf1b71d1be354e64c649a310d8c914ede266f7
小强 <shuntian@Mac.lan>
2026-01-04 11:32:55 +0800

    update deps

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
@@ -6,7 +6,6 @@ const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMi
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const redirectServedPath = require('react-dev-utils/redirectServedPathMiddleware');
 const paths = require('./paths');
-const getHttpsConfig = require('./getHttpsConfig');
 
 const host = process.env.HOST || '0.0.0.0';
 const sockHost = process.env.WDS_SOCKET_HOST;
@@ -91,7 +90,6 @@ module.exports = function (proxy, allowedHost) {
       publicPath: paths.publicUrlOrPath.slice(0, -1),
     },
 
-    https: getHttpsConfig(),
     host,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
@@ -121,7 +119,7 @@ module.exports = function (proxy, allowedHost) {
         // it used the same host and port.
         // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
         noopServiceWorkerMiddleware(paths.publicUrlOrPath)
-      )
+      );
       return middlewares;
     },
   };
